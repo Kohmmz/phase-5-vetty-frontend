6bf8533dd7b3137e3c5a113df0920f6e201c4f0e
Victor <victor.kihiko@student.moringaschool.com>
2025-05-13 07:54:02 +0300

    vercel

--- vetty-frontend/src/Components/api/api.jsx
+++ vetty-frontend/src/Components/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // import checkout from './checkout';
-const API_URL = 'https://backend-testing-main.onrender.com';
+const API_URL = '/api';
 
 const api = axios.create({
   baseURL: API_URL,
